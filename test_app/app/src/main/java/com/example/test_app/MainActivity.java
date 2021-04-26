1aa9629fbe9421cb0f1540fc0f60b37f894284d3
hyemin <yuhyemin96@gmail.com>
2021-04-26 23:57:52 +0900

    notification(push alram)

--- test_app/app/src/main/java/com/example/test_app/MainActivity.java
+++ test_app/app/src/main/java/com/example/test_app/MainActivity.java
@@ -1,20 +1,47 @@
 package com.example.test_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.test.MESSAGE";
 
+    // https://androman.tistory.com/105
+    // 채널 생성하기
+    private static final String PRIMARY_CHANNEL_ID = "notify_channel";
+    private NotificationManager notificationManager;
+    // Notification에 대한 ID 생성
+    private static final int NOTIFICATION_ID = 0;
+    // Notification을 호출 할 button 변수
+    private Button button_notify;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // push 알람 버튼 listener 등록
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
+        createNotificationChannel();
+
     }
 
     /** Send 버튼 클릭시 호출*/
@@ -33,5 +60,47 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
+    }
+
+    // 채널 생성 메소드
+    private void createNotificationChannel() {
+        try{
+            notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                // Channel 정의 생성자 (채널 아이디, 채널 이름, 중요도)
+                NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID, "Test", notificationManager.IMPORTANCE_HIGH);
+                // Channel 에 대한 기본 설정
+                notificationChannel.enableLights(true);
+                notificationChannel.setLightColor(Color.RED);
+                notificationChannel.enableVibration(true);
+                notificationChannel.setDescription("테스트다");
+
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    // Notification Builder 생성 후 NotificationManager 로 전달하여 호출
+    public void sendNotification(){
+        try{
+            // 알림 탭 설정
+            Intent intent = new Intent(this, NotificationDetail.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_notificatioin_watch)
+                    .setContentTitle("커밋해야지")
+                    .setContentText("커밋해야지")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+            notificationManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
